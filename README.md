e605c3efe857bd2b6ccae43e536c60b0726e62c8
gatewayitconsultingDesktop <43389272+gatewayitconsulting@users.noreply.github.com>
2022-08-04 19:54:10 -0500

    Usability improvements

--- README.md
+++ README.md
@@ -38,3 +38,14 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+## Feature Roadmapping
+
+- Add ability to change tax rate applied from default of ~30%.
+
+    - Allow user to input custom percentage.
+
+- Add ability to email results to self or another.
+
+- Add number formatting. i.e. $1,500.00
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-// import React, { useState, useEffect } from 'react';
-
-
+import Head from 'next/head';
+import Image from 'next/image';
+import React, { useEffect } from 'react';
+import styles from '../styles/Home.module.css';
 
 
 export default function Home() {
-  // function clearResult() {
-  //   const salary = document.querySelector('#salary').value;
-  //   const money = document.getElementById('#money');
-
-  //   if (!salary) {
-  //     money.style.display("none");
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   clearResult();
-  // });
+  useEffect(() => {
+    const salaryInput = document.getElementById('salary');
+    salaryInput.focus();
+  }, []);
 
   function calculateHousingSalaryCap(event) {
     const salary = document.querySelector('#salary').value;
+    const salaryInput = document.getElementById('salary');
+    const showCap = document.getElementById('showCap');
     var mortgageLimit = 0;
     var mortgageFactor = 0.3;
     var calculatedSalary = 0;
@@ -29,17 +21,38 @@ export default function Home() {
     var taxDifference = 0;
     var monthlyIncome = 0;
     var months = 12;
+    var defaultValues = '0.00';
 
     if (!salary) {
       alert('Please enter your salary.');
+      salaryInput.focus();
+      event.preventDefault();
     } else {
       calculatedSalary = salary * taxFactor;
       taxDifference = salary - calculatedSalary;
       monthlyIncome = taxDifference / months;
       mortgageLimit = monthlyIncome * mortgageFactor;
-      document.getElementById('showCap').innerHTML = mortgageLimit;
-      event.preventDefault();
+
+      if (isNaN(mortgageLimit)) {
+        alert('Please only enter numbers.');
+        showCap.innerHTML = defaultValues;
+        event.preventDefault();
+      } else {
+        showCap.innerHTML = mortgageLimit.toFixed(2);
+        event.preventDefault();
+      }
     }
+  }
+
+  function clearResults(event) {
+    const salaryInput = document.getElementById('salary');
+    const showCap = document.getElementById('showCap');
+    var defaultValues = '0.00';
+
+    showCap.innerHTML = defaultValues;
+    salaryInput.value = '';
+    salaryInput.focus();
+    event.preventDefault();
   }
 
   return (
@@ -51,11 +64,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className='row mb-5'>
+        <div className='row mb-4'>
           <div className='col-12 col-md-6 mx-auto text-center'>
             <h1>House Salary Calculator</h1>
             <p>
               The point of this calculator is to make sure you are looking at a safe maximum to pay monthly for a mortgage using the salary you are at or planning to have.
+            </p>
+            <p>
+              The calculator also works the same for rent.
             </p>
             <p>
               <em>
@@ -66,18 +82,29 @@ export default function Home() {
         </div>
         <div className='row'>
           <form>
-            <label for="salary" className='col-12 text-center mb-3 form-label'>Salary:</label>
-            <input type="text" name="salary" id="salary" className='col-12 text-center form-control' />
-            <button type="submit" onClick={calculateHousingSalaryCap} className='col-12 btn btn-light mt-3'>
+            <label htmlFor="salary" className='col-12 text-center mb-3 form-label'>Salary:</label>
+            <input type="text" name="salary" id="salary" className='col-12 text-center form-control' placeholder='Enter salary' />
+            <button type="submit" onClick={calculateHousingSalaryCap} className='col-12 btn btn-outline-light mt-3'>
               Calculate
+            </button>
+            <button type="submit" onClick={clearResults} className='col-12 btn btn-outline-light mt-3'>
+              Clear results
             </button>
           </form>
         </div>
         <div className='row mt-5'>
           <div className='col-12'>
-            <p className={styles.white}>
-              <span id='money'>Monthly Payment Max after Tax: $</span>
-              <span id='showCap' className={styles.bold}></span>
+            <p className={styles.monthlySummary}>
+              <span id='money'>Monthly payment max after tax calculations: $</span>
+              <span id='showCap' className={styles.bold}>0.00</span>
+            </p>
+          </div>
+        </div>
+
+        <div className='row mt-5'>
+          <div className='col-12 col-md-6 mx-auto'>
+            <p className={styles.disclaimer}>
+              *You are responsible for your own investment decisions. House Salary Calculator will not be responsible for any errors or omissions in articles or postings, for hyperlinks embedded in messages, or for any results obtained from the use of such information. House Salary Calculator will not be liable for any loss or damage caused by a reader's reliance on information obtained in our assets. If you don't accept this responsibility for yourself, then you should not use House Salary Calculator.
             </p>
           </div>
         </div>
